d7b4ef6487400e4af78c8188a71b15c4d69981d2
jlshaw117 <jlshaw117@gmail.com>
2018-11-01 13:59:17 -0700

    Refactor function for better time complexity

--- execution_time_difference.rb
+++ execution_time_difference.rb
@@ -1,4 +1,4 @@
-def my_min(list)
+def my_min_a(list)
   dup_list = list.dup
   list.each do |number_1|
     # dup_list = list.dup
@@ -11,3 +11,16 @@ def my_min(list)
   end
   dup_list[0]
 end
+
+def my_min_b(list)
+  min = list.first
+  list.each do |num|
+    if num < min 
+      min = num 
+    end
+  end
+  min
+end
+list = [ 0, 3, 5, 4, -5, 10, 1, 90 ].concat((1..10000).to_a)
+my_min_b(list)
+
