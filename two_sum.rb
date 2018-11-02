6864ce732edc9e65fabd291f81aa9c9932a1233b
jlshaw117 <jlshaw117@gmail.com>
2018-11-01 17:33:27 -0700

    Add bad_two_sum

---
+++ two_sum.rb
@@ -0,0 +1,11 @@
+def bad_two_sum(arr,target)
+  (0...arr.length).each do |i|
+    (i+1...arr.length).each do |j|
+      return true if arr[i] + arr[j] == target
+    end
+  end
+  false
+end
+arr = (1..10000).to_a
+
+p bad_two_sum(arr, 1000000000000)
