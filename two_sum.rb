e860421ce672bd9177fc7d8fcf39e2c35f1239ca
jlshaw117 <jlshaw117@gmail.com>
2018-11-01 18:09:57 -0700

    last commit

--- two_sum.rb
+++ two_sum.rb
@@ -7,5 +7,9 @@ def bad_two_sum(arr,target)
   false
 end
 arr = (1..10000).to_a
+# 
+# p bad_two_sum(arr, 1000000000000)#~7secs
 
-p bad_two_sum(arr, 1000000000000)
+def okay_two_sum?(arr,target)
+  new_arr = arr.sort
+end
